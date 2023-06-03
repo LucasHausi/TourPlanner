d2b93d21143052bce617017df48db9074ebb635a
if22b189 <philipp.winkler@technikum-wien.at>
2023-06-03 14:36:51 +0200

    add needed dependencies to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.tourplanner.tourplanner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
+    requires java.persistence;
+    requires spring.context;
+    requires spring.data.jpa;
 
 
     opens com.tourplanner.tourplanner to javafx.fxml;
