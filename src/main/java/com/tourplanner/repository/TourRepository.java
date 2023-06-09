0a328df412aa0f3a7bfe8f970d84648a9282f683
LucasHausi <l.hausensteiner@gmail.com>
2023-06-09 11:22:56 +0200

    Try to implement tests for a repo

--- src/main/java/com/tourplanner/repository/TourRepository.java
+++ src/main/java/com/tourplanner/repository/TourRepository.java
@@ -3,8 +3,10 @@ import com.tourplanner.model.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour, UUID>{
+    Tour findByUUID(UUID id);
 }
---
+++ src/test/java/com/tourplanner/repository/TourRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.tourplanner.repository;
+
+import com.tourplanner.model.Tour;
+import com.tourplanner.model.TransportType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class TourRepositoryTest {
+    @Autowired
+    private TourRepository repo;
+    private Tour tour1;
+    @BeforeEach
+    void setUp() {
+        tour1 = new Tour(UUID.randomUUID(), "TestRoute","Go north","Westfield",
+                "Northfield", TransportType.BIKE, 22.0, "22:00", "Image ---");
+        repo.save(tour1);
+    }
+
+    @Test
+    void findByUUID() {
+        var resultTour = repo.findByUUID(tour1.getId());
+        assertNotNull(resultTour);
+        assertEquals(tour1,resultTour);
+    }
+}
