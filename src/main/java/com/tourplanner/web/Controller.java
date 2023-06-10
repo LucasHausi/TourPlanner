072f2fcc85b984dc1cc12c470d21b4f08c37dd41
if22b189 <philipp.winkler@technikum-wien.at>
2023-06-10 14:33:16 +0200

    add newTour restMethod

--- src/main/java/com/tourplanner/web/Controller.java
+++ src/main/java/com/tourplanner/web/Controller.java
@@ -1,7 +1,24 @@
 package com.tourplanner.web;
+import com.tourplanner.model.Tour;
+import com.tourplanner.repository.TourRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
+    public final TourRepository tourRepository;
 
+    public Controller(TourRepository tourRepository) {
+        this.tourRepository = tourRepository;
+    }
+
+    @PostMapping(path = "/newTour")
+    public ResponseEntity<Tour> newTour(@RequestBody Tour tour) {
+        Tour savedTour = tourRepository.save(tour);
+        //String path = "/newCar";
+        //URI uri = ServletUriComponentsBuilder.fromCurrentRequest().replacePath(path).build(savedTour);
+        return ResponseEntity.ok().body(savedTour);
+    }
 }
