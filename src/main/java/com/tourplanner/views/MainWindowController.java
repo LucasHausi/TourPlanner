c55182a84c40859d54ec3b167d3364227e97a200
LucasHausi <l.hausensteiner@gmail.com>
2023-06-10 21:43:20 +0200

    FXML Update

--- src/main/java/com/tourplanner/views/MainWindowController.java
+++ src/main/java/com/tourplanner/views/MainWindowController.java
@@ -2,8 +2,9 @@ package com.tourplanner.views;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,9 +12,16 @@ import java.util.ResourceBundle;
 public class MainWindowController implements Initializable {
     @FXML
     public TextField searchField;
+    @FXML
+    ListView<String> listView = new ListView();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         searchField.setText("Find me Nemo!");
+        listView.getItems().add("Initial");
+    }
+
+    public void addItem(){
+        listView.getItems().add("New");
     }
 }
