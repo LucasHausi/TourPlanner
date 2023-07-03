0dc7cf1c90db1f42fa5755ed3eb1e94d944b1870
if22b189 <philipp.winkler@technikum-wien.at>
2023-07-03 09:00:15 +0200

    introduce logging for backend RestControllers

--- src/main/java/com/tourplanner/backend/web/TourController.java
+++ src/main/java/com/tourplanner/backend/web/TourController.java
@@ -5,6 +5,8 @@ import com.tourplanner.backend.dal.repository.TourLogRepository;
 import com.tourplanner.backend.dal.repository.TourRepository;
 import com.tourplanner.backend.mapper.TourMapper;
 import com.tourplanner.shared.model.Tour;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,16 @@ public class TourController {
     private final TourLogRepository tourLogRepository;
     private final TourMapper tourMapper;
 
+    private static Logger logger;
+    static {
+        try {
+            // you need to do something like below instaed of Logger.getLogger(....);
+            logger = LogManager.getLogger(TourController.class);
+        } catch (Throwable th) {
+            throw new IllegalArgumentException("Cannot load the log property file", th);
+        }
+    }
+
     public TourController(TourRepository tourRepository, TourLogRepository tourLogRepository, TourMapper tourMapper) {
         this.tourRepository = tourRepository;
         this.tourLogRepository = tourLogRepository;
@@ -32,27 +44,33 @@ public class TourController {
         TourEntity savedTourEntity = tourRepository.save(tourMapper.toEntity(tour));
         String path = "/tour/"+ savedTourEntity.getId();
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().replacePath(path).build(savedTourEntity);
+        logger.info("Sucessfully created or updated tour: " + savedTourEntity.getId());
         return ResponseEntity.created(uri).body(tourMapper.fromEntity(savedTourEntity));
     }
     @DeleteMapping(value = "/tour/{tourId}/delete")
     public ResponseEntity<UUID> deleteTour(@PathVariable UUID tourId) {
         if (!tourRepository.existsById(tourId)) {
+            logger.warn("Deletion not performed, Tour " + tourId + " could not be found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         for(TourLogEntity tourLog : tourLogRepository.getTourLogEntityByTourEntity(tourRepository.findById(tourId).orElseThrow())){
+            logger.info("Deleting TourLog: " + tourLog.getId() + " of Tour: " + tourId);
             tourLogRepository.deleteById(tourLog.getId());
         }
+        logger.info("Deleting Tour: " + tourId);
         tourRepository.deleteById(tourId);
         return new ResponseEntity<>(tourId, HttpStatus.OK);
     }
 
     @GetMapping(path = "/tour/{id}")
     public Tour getTour(@PathVariable("id") UUID id){
+        logger.info("Fetching Tour: " + id);
         return tourMapper.fromEntity(tourRepository.findById(id).orElseThrow());
    }
 
     @GetMapping(path = "/allTours")
     public List<Tour> getAllTours(){
+        logger.info("Fetching all existing Tours");
         return tourRepository.findAll().stream().map(tourMapper::fromEntity)
                 .collect(Collectors.toList());
     }
--- src/main/java/com/tourplanner/backend/web/TourLogController.java
+++ src/main/java/com/tourplanner/backend/web/TourLogController.java
@@ -4,6 +4,8 @@ import com.tourplanner.backend.dal.repository.TourLogRepository;
 import com.tourplanner.backend.dal.repository.TourRepository;
 import com.tourplanner.backend.mapper.TourLogMapper;
 import com.tourplanner.shared.model.TourLog;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,16 @@ public class TourLogController {
 
     public final TourLogMapper tourLogMapper;
 
+    private static Logger logger;
+    static {
+        try {
+            // you need to do something like below instaed of Logger.getLogger(....);
+            logger = LogManager.getLogger(TourLogController.class);
+        } catch (Throwable th) {
+            throw new IllegalArgumentException("Cannot load the log property file", th);
+        }
+    }
+
     public TourLogController(TourLogRepository tourLogRepository, TourRepository tourRepository, TourLogMapper tourLogMapper) {
         this.tourLogRepository = tourLogRepository;
         this.tourRepository = tourRepository;
@@ -29,34 +41,40 @@ public class TourLogController {
 
     @PostMapping(path = "/tourLog/save")
     public ResponseEntity<TourLog> createOrUpdateTourLog(@RequestBody TourLog tourLog) {
-        TourLogEntity savedTour = tourLogRepository.save(tourLogMapper.toEntity(tourLog));
-        String path = "/tour/"+savedTour.getId();
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().replacePath(path).build(savedTour);
-        return ResponseEntity.created(uri).body(tourLogMapper.fromEntity(savedTour));
+        TourLogEntity savedTourLogEntity = tourLogRepository.save(tourLogMapper.toEntity(tourLog));
+        String path = "/tour/"+savedTourLogEntity.getId();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().replacePath(path).build(savedTourLogEntity);
+        logger.info("Sucessfully created or updated tourLog: " + savedTourLogEntity.getId());
+        return ResponseEntity.created(uri).body(tourLogMapper.fromEntity(savedTourLogEntity));
     }
 
     @GetMapping(path = "/tourLog/{id}")
     public TourLog getTourLog(@PathVariable("id") UUID id){
+        logger.info("Fetching TourLog: " + id);
         return tourLogMapper.fromEntity(tourLogRepository.findById(id).orElseThrow());
    }
 
     @DeleteMapping(value = "/tourLog/{tourLogId}/delete")
     public ResponseEntity<UUID> deleteTourLog(@PathVariable UUID tourLogId) {
         if (!tourLogRepository.existsById(tourLogId)) {
+            logger.warn("Deletion not performed, TourLog " + tourLogId + " could not be found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        logger.info("Deleting TourLog: " + tourLogId);
         tourLogRepository.deleteById(tourLogId);
         return new ResponseEntity<>(tourLogId, HttpStatus.OK);
     }
 
     @GetMapping(path = "/tour/{tourId}/tourLogs")
     public List<TourLog> getTourLogs(@PathVariable("tourId") UUID tourId){
+        logger.info("Fetching all TourLogs of Tour: " + tourId);
         return tourLogRepository.getTourLogEntityByTourEntity(tourRepository.findById(tourId).get())
                 .stream().map(tourLogMapper::fromEntity).collect(Collectors.toList());
     }
 
     @GetMapping(path = "/tourLogs")
     public List<TourLog> getAllTourLogs(){
+        logger.info("Fetching all existing TourLogs");
         return tourLogRepository.findAll().stream().map(tourLogMapper::fromEntity)
                 .collect(Collectors.toList());
     }
